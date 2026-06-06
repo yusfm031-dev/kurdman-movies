f602160fbdd27014c6d866c3da611f7559504911
yusfm031-dev <yusfm031@gmail.com>
2026-06-07 00:45:39 +0300

    Update movies.js

--- movies.js
+++ movies.js
@@ -6,45 +6,50 @@ quality:"1080P",
 video:"https://vz-486dbc69-7fb.b-cdn.net/74179f2f-20ff-4eb7-92e0-f06b725dbc9d/playlist.m3u8"
 },
 {
-title:"عەمۆ و ڕەفتە",
+title:"عەشق و فیتنە",
 image:"file_00000000e440720a9828825168bd0226.png",
 quality:"8.5",
-video:"https://..."
+video:"https://vz-5ea6d1f8-2d5.b-cdn.net/aeb5ca24-5b0d-4f87-a5a6-ef2f8d6fb168/playlist.m3u8"
 },
+
 {
 title:"ساوا",
 image:"file_00000000b37872469944d35385c90010.png",
 quality:"8.3",
-video:"https://vz-xxxxxxxx/playlist.m3u8"
-}
+video:"https://vz-908031df-65a.b-cdn.net/c6810e64-9623-43c7-ad52-566b3cc1f02b/playlist.m3u8"
+},
+
 {
-title:"کەرش",
-image:"file_00000001d8f71f49685c5a6ef22e96.png",
+title:"رۆستەم",
+image:"file_000000001dd871f49685c5a6ef226e96.png",
 quality:"8.1",
-video:"https://..."
+video:"https://vz-62e66ed7-6fe.b-cdn.net/edf3202f-9dee-476e-a52a-a3ad0039795e/playlist.m3u8"
 },
+
 {
-title:"لێبی",
-image:"file_00000006ec871f4a20f395a91e29602.png",
+title:"لمبی",
+image:"file_000000006ec871f4a20f395a91e29602.png",
 quality:"8.9",
-video:"https://..."
+video:"https://vz-84ab955f-09b.b-cdn.net/d7aeff2c-c8ad-4e92-8400-789df53f7991/playlist.m3u8"
 },
+
 {
 title:"فیلمی نوێ",
 image:"image_1779992223418.jpeg",
 quality:"9.1",
-video:"https://..."
+video:"https://vz-18bdfcb2-789.b-cdn.net/b3e8d1b4-5a43-4a19-87a1-a0b7073a6f85/playlist.m3u8"
 },
+
 {
-title:"گیل و کیش",
+title:"گیل و گیتەش",
 image:"file_00000000746c71f4a4b1982c67e3de2d.png",
 quality:"8.8",
-video:"https://..."
+video:"https://vz-76f7682b-70b.b-cdn.net/71dd4fe9-8435-4465-a72d-38c1b3681501/playlist.m3u8"
 },
+
 {
 title:"ئەلیۆ",
 image:"file_000000007c14720ab85b3d3eaba07005.png",
 quality:"9.0",
 video:"elio.html"
 }
-];
