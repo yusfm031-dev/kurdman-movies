11ca1271526543a50a9fb86de1d4f3c8daa94a25
yusfm031-dev <yusfm031@gmail.com>
2026-06-07 00:09:31 +0300

    Update movies.js

--- movies.js
+++ movies.js
@@ -4,5 +4,47 @@ title:"هۆبس و شۆو",
 image:"file_00000000bb8471f4931cdde8b891f1c8.png",
 quality:"1080P",
 video:"https://vz-486dbc69-7fb.b-cdn.net/74179f2f-20ff-4eb7-92e0-f06b725dbc9d/playlist.m3u8"
+},
+{
+title:"عەمۆ و ڕەفتە",
+image:"file_00000000e440720a9828825168bd0226.png",
+quality:"8.5",
+video:"https://..."
+},
+{
+title:"ساوا",
+image:"file_00000000b37872469944d35385c90010.png",
+quality:"8.3",
+video:"https://..."
+},
+{
+title:"کەرش",
+image:"file_00000001d8f71f49685c5a6ef22e96.png",
+quality:"8.1",
+video:"https://..."
+},
+{
+title:"لێبی",
+image:"file_00000006ec871f4a20f395a91e29602.png",
+quality:"8.9",
+video:"https://..."
+},
+{
+title:"فیلمی نوێ",
+image:"image_1779992223418.jpeg",
+quality:"9.1",
+video:"https://..."
+},
+{
+title:"گیل و کیش",
+image:"file_00000000746c71f4a4b1982c67e3de2d.png",
+quality:"8.8",
+video:"https://..."
+},
+{
+title:"ئەلیۆ",
+image:"file_000000007c14720ab85b3d3eaba07005.png",
+quality:"9.0",
+video:"elio.html"
 }
 ];
