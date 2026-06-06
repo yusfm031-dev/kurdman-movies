5063f11e78cc1808e330732e704f05e9af6bcffc
yusfm031-dev <yusfm031@gmail.com>
2026-06-06 23:20:11 +0300

    Create movies.js

---
+++ movies.js
@@ -0,0 +1,8 @@
+const movies = [
+{
+title:"هۆبس و شۆو",
+image:"file_00000000bb8471f4931cdde8b891f1c8.png",
+quality:"1080P",
+video:"https://vz-486dbc69-7fb.b-cdn.net/74179f2f-20ff-4eb7-92e0-f06b725dbc9d/playlist.m3u8"
+}
+];
