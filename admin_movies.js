cbbd91857d22a7aba2f2418e6613038331efd659
yusfm031-dev <yusfm031@gmail.com>
2026-06-29 02:11:26 +0300

    Create admin_movies.js

---
+++ admin_movies.js
@@ -0,0 +1,75 @@
+async function loadMovies(){
+
+    const res = await fetch("https://kurdmantv.com/kurdmantv_api/get_movies.php");
+
+    const movies = await res.json();
+
+    let html = "";
+
+    movies.forEach(movie=>{
+
+        html += `
+        <div class="movie-card">
+
+            <img src="${movie.poster}" width="80">
+
+            <div class="info">
+
+                <h3>${movie.title}</h3>
+
+                <p>${movie.category}</p>
+
+                <p>${movie.year}</p>
+
+                <p>${movie.vip==1?"👑 VIP":"🆓 Free"}</p>
+
+            </div>
+
+            <div>
+
+                <button onclick="editMovie(${movie.id})">
+                ✏️ Edit
+                </button>
+
+                <button onclick="deleteMovie(${movie.id})">
+                🗑 Delete
+                </button>
+
+            </div>
+
+        </div>
+        `;
+
+    });
+
+    document.getElementById("movieList").innerHTML = html;
+
+}
+
+loadMovies();
+
+function editMovie(id){
+
+    alert("Edit Movie ID : "+id);
+
+}
+
+async function deleteMovie(id){
+
+    if(!confirm("Delete Movie?")) return;
+
+    await fetch("https://kurdmantv.com/kurdmantv_api/delete_movie.php",{
+
+        method:"POST",
+
+        headers:{
+            "Content-Type":"application/x-www-form-urlencoded"
+        },
+
+        body:"id="+id
+
+    });
+
+    loadMovies();
+
+}
